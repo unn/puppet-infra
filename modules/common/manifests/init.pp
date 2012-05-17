f78306eaad2b3c0d8de0d1329f7ee1070b00716f
David Stoline <drupal@davidstoline.com>
2012-05-17 02:34:44 -0400

    Some refactoring based on puppet-lint

--- modules/common/manifests/init.pp
+++ modules/common/manifests/init.pp
@@ -1,42 +1,56 @@
 class common {
-  group { "puppet":
-    ensure => "present",
+  group { 'puppet':
+    ensure => 'present',
+  }
+
+  apt::source { 'percona':
+    location          => 'http://repo.percona.com/apt',
+    release           => 'squeeze',
+    repos             => 'main',
+@    required_packages => 'libmysqlclient18=5.5.23-rel25.3-240.squeeze',
+    key               => 'CD2EFD2A',
+    key_server        => 'keys.gnupg.net',
+    include_src       => '1C4CBDCDCD2EFD2A',
+  }
+
+  apt::source { 'dotdeb':
+    location          => 'http://packages.dotdeb.org',
+    release           => 'squeeze',
+    repos             => 'all',
+    key               => 'E9C74FEEA2098A6E',
+    key_source        => 'http://www.dotdeb.org/dotdeb.gpg',
+    require           => [
+      Package['wget'],
+      Apt::Source['percona'],
+    ],
   }
 
   package {[
-      "htop",
-      "curl",
-      "wget",
-      "rsync",
-      "git",
-      "vim",
-      "pv",
-      "tree",
+      'htop',
+      'curl',
+      'rsync',
+      'wget',
+      'git',
+      'vim',
+      'pv',
+      'tree',
     ]:
-    ensure => latest;
+    ensure => latest,
   }
-  
-#  apt::source { "percona":
-#    location          => "http://repo.percona.com/apt",
-#    release           => "squeeze",
-#    repos             => "main",
-#    key               => "55BE302B",
-#    key_server        => "keys.gnupg.net",
-#    include_src       => CD2EFD2A
-#  }
-  
-#  apt::source { "dotdeb":
-#    location          => "http://packages.dotdeb.org",
-#    release           => "squeeze",
-#    repos             => "all",
-#  }
-  
+
+  package {[
+      'percona-xtradb-cluster-client-5.5',
+      'libmysqlclient18=5.5.23-rel25.3-240.squeeze',
+    ]:
+    require => Apt::Source['percona'],
+  }
+
   cron {
-    "apt-update":
-      command => "/usr/bin/apt-get update",
-        user    => root,
-        hour    => 3,
-        minute  => 25;
+    'apt-update':
+      command => '/usr/bin/apt-get update',
+      user    => root,
+      hour    => 3,
+      minute  => 25,
   }
 
 }
